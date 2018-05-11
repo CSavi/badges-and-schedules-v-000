ad70acc346921732ee2e4abd9d12e01350981d74
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 14:48:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(speakers)
   
   speakers.each_with_index do |name, index|
   new_array << "Hello, #{name}. You'll be assigned to room #{room_numbers}."
-  room_numbers = room_numbers + 1
+  room_numbers +=
   end 
   new_array
 end 
