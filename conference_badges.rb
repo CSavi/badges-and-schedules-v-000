58df4ed84c07442202aaf3116db864a0a3d28d9e
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 14:51:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(speakers)
   new_array << "Hello, #{name}. You'll be assigned to room #{room_numbers}."
   room_numbers += 1 
 end   
-  new_array
+  return new_array
 end 
    
 def printer(array)
