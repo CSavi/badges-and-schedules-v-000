9fcc376f6fbb7f35a3a1db8ce4178937ebbe12a2
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 14:54:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,10 @@ def batch_badge_creator(speakers)
 end   
 
 def assign_rooms(speakers)
-  room_numbers = 0
+  #room_numbers = 1
   new_array = []
   speakers.each_with_index do |name, index|
-  new_array << "Hello, #{name}. You'll be assigned to room #{room_numbers}."
+  new_array << "Hello, #{name}. You'll be assigned to room #{index}."
   #room_numbers += 1 
 end   
   new_array
